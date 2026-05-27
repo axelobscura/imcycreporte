32935fa2b14103c78bb7dfbc9fe4ad8de18de5b9
Axel Laurent <axosar@gmail.com>
2026-05-27 10:53:54 -0600

    entrada

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -1,14 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Panel() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Image src="/logo-imcyc.svg" alt="Logo" width={300} height={100} className="mb-4" />
       <p>Instituto Mexicano del Cemento y del Concreto A.C.</p>
       <h1 className="text-4xl font-bold">MODELO ESTADÍSTICO</h1>
       <h3 className="text-lg font-bold">PLANTAS DE CONCRETO</h3>
-      <Link href="/panel" className="bg-gray-900 text-white px-10 py-2 rounded-md mt-5">INGRESAR</Link>
     </div>
   );
 }
