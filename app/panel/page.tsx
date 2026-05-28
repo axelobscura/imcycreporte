6106dac8082d8e5707d6a52fe346fb4324588d05
Axel Laurent <axosar@gmail.com>
2026-05-28 10:36:56 -0600

    fields

--- app/panel/page.tsx
+++ app/panel/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+
 
 export default function Panel() {
   const [variables, setVariables] = useState<{ id: number, name: string, value: number | string}[]>([]);
@@ -19,32 +21,30 @@ export default function Panel() {
         <h3 className="text-sm font-bold text-center text-white">PLANTAS DE CONCRETO</h3>
       </div>
       <div className="bg-slate-700 p-5">
-        <h2 className="text-2xl font-bold mb-5">Ingresar los valores correspondientes a la mezcla de concreto</h2>
-        <p className="mb-4">Suma de variables: {valorx}</p>
+        <h2 className="text-2xl font-bold mb-5"><FaRegArrowAltCircleRight className="inline-block mr-2" /> Ingresar los valores correspondientes a las muestras de la planta</h2>
+        <p className="mb-4 text-xl font-bold">{valorx > 0 ? 'Suma de valores: ' + valorx : 'Por favor Ingrese los valores: '}</p>
         <div className="grid grid-cols-1">
           <div className="flex flex-col">
             {variables.map((variable) => (
-              <div key={variable.id}>
-                <label htmlFor={variable.id.toString()}>{variable.name}</label>
-                <input type="number" id={variable.id.toString()} placeholder={variable.name} value={variable.value} onChange={(e) => setVariables(variables.map(v => v.id === variable.id ? { ...v, value: Number(e.target.value) } : v))} />
+              <div key={variable.id} className="flex flex-col gap-2 my-1">
+                <label htmlFor={variable.id.toString()} className="text-lg font-bold">{variable.name}:</label>
+                <input type="number" id={variable.id.toString()} placeholder={variable.name} value={variable.value} onChange={(e) => setVariables(variables.map(v => v.id === variable.id ? { ...v, value: Number(e.target.value) } : v))} className="bg-gray-800 text-white px-4 py-2" />
               </div>
             ))}
           </div>
           {/* buttons to add and remove variables */}
-          <div className="flex flex-row gap-2">
-            {variables.length > 0 && (
-              <button className="bg-gray-900 text-white px-4 py-2 rounded-md" onClick={() => setVariables(variables.slice(0, -1))}>Remover variable</button>
-            )}
+          <div className="flex flex-row gap-2 w-full my-3">
             {/* add variable button */}
-            <button className="bg-gray-900 text-white px-4 py-2 rounded-md" onClick={() => setVariables([...variables, { id: variables.length + 1, name: `Variable ${variables.length + 1}`, value: '' }])}>Agregar variable</button>
+              <button className="bg-green-900 text-white px-4 py-2 uppercase" onClick={() => setVariables([...variables, { id: variables.length + 1, name: `Valor ${variables.length + 1}`, value: '' }])}>Agregar valor</button>
+            {variables.length > 0 && (
+              <button className="bg-red-900 text-white px-4 py-2 uppercase" onClick={() => setVariables(variables.slice(0, -1))}>Eliminar valor</button>
+            )}
+            
           </div>
         </div>
         <div>
           {valorx > 0 && (
             <p>El valor de x es: {valorx / variables.length}</p>
-          )}
-          {valorx <= 0 && (
-            <p>El valor de x es: 0</p>
           )}
         </div>
       </div>
